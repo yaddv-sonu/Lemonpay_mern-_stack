fc7cf1a5e33b5cecad7da5d0b74d856c835f5be2
yaddv-sonu <sonu.yadav@ptuiv.edu.in>
2025-05-05 20:37:07 +0530

    feature : write the authMiddlerware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,17 @@
 
-const authMiddleware = function (req, res, next) {
+import jwt from 'jsonwebtoken';
 
+const authMiddleware = function (req, res, next) {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ msg: 'No token, auth denied' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
 };
 
 export default authMiddleware;
