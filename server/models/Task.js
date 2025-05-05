0f656b2f3707369d4403ba2a982569b4aeec8922
yaddv-sonu <sonu.yadav@ptuiv.edu.in>
2025-05-05 20:19:38 +0530

    feat: add User and Task schemas in models
    - Created User schema with fields: name, email, password, etc.
    - Created Task schema with fields: title, description, dueDate,
    - Added timestamps and necessary validations

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
-  
- });
-
-
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  taskName: { type: String, required: true },
+  description: { type: String },
+  dueDate: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
 
 export default mongoose.model('Task', taskSchema);
